e6f19af81101978b7a363bb9fe26bb61a6cb7a6e
Matheus <matheus.souza@essencislabs.com>
2025-08-13 17:10:11 -0300

    Ajuste do Toast - Username or password incorret!

--- front/src/app/account/login/toasts-container.component.ts
+++ front/src/app/account/login/toasts-container.component.ts
@@ -2,6 +2,7 @@ import { Component, TemplateRef } from '@angular/core';
 
 import { ToastService } from './toast-service';
 import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
@@ -14,22 +15,21 @@ import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
       [delay]="toast.delay || 5000"
       (hidden)="toastService.remove(toast)"
     >
-      <ng-template [ngIf]="isTemplate(toast)" [ngIfElse]="text">
-        <ng-template [ngTemplateOutlet]="toast.textOrTpl"></ng-template>
-      </ng-template>
-
-      <ng-template #text>{{ toast.textOrTpl }}</ng-template>
+      {{ toast.textOrTpl }}
     </ngb-toast>
    }
   `,
     host: { 'class': 'toast-container position-fixed top-0 end-0 p-3', 'style': 'z-index: 1200' },
     standalone: true,
     imports: [
-      NgbToastModule
+      NgbToastModule,
+      CommonModule
     ]
 })
 export class ToastsContainer {
   constructor(public toastService: ToastService) { }
 
-  isTemplate(toast: { textOrTpl: any; }) { return toast.textOrTpl instanceof TemplateRef; }
+  isTemplate(toast: { textOrTpl: any; }) { 
+    console.log(toast);
+    return toast.textOrTpl instanceof TemplateRef; }
 }
